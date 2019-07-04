0274ef74911859e8bf25c65eed7496504df0ac69
Sergei Chestakov <schestakov@ucdavis.edu>
2019-07-04 01:26:16 -0700

    Recursively read files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
+use std::env;
 use std::io::Write;
 use std::path::Path;
+use std::fs::{self, DirEntry};
 
 extern crate rpassword;
 
@@ -8,6 +10,7 @@ fn main() {
     if !Path::new(".git").is_dir() {
         panic!("Must be in a git directory!");
     }
+    let _args: Vec<String> = env::args().collect();
 
     println!("Please enter your Github credentials.");
     print!("Username: ");
@@ -18,4 +21,21 @@ fn main() {
     io::stdin().read_line(&mut username)
         .expect("Failed to read line");
     let _pass = rpassword::read_password_from_tty(Some("Password: ")).unwrap();
+
+    read_files(Path::new("./"));
+}
+
+fn read_files(dir: &Path) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                read_files(&path)?;
+            } else {
+                println!("{}", path.display());
+            }
+        }
+    }
+    Ok(())
 }
