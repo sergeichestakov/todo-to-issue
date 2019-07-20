d3c2cb9c4cf74e18b7d4317d5685e06013d1a53c
Sergei Chestakov <schestakov@ucdavis.edu>
2019-07-19 19:45:09 -0700

    Move file reading logic to parse.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
 use std::path::Path;
 
 use request::Request;
@@ -8,7 +6,7 @@ mod request;
 mod command;
 mod parse;
 
-fn main() -> io::Result<()> {
+fn main() {
     if !Path::new(".git").is_dir() {
         panic!("Must be in a git directory!");
     }
@@ -17,24 +15,8 @@ fn main() -> io::Result<()> {
     let files = command::get_tracked_files();
 
     for path in files {
-        let file = File::open(&path)?;
-        let buffer = BufReader::new(file);
-
-        let mut line_number = 0;
-        for line_option in buffer.lines() {
-            let line = line_option.unwrap();
-            line_number += 1;
-
-            if parse::contains_todo(&line) {
-                let params = Request::build_params(
-                    parse::extract_title(&line),
-                    parse::create_description(&line_number, &path)
-                );
-                let result = request.create_issue(params);
-                println!("{:?}", result);
-            }
+        if let Err(e) = parse::read_file(&path, &request) {
+            println!("Failed to read file {}. Received error {}", path, e);
         }
     }
-
-    Ok(())
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,8 +1,37 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 use std::str;
+
+use super::parse;
+use super::request;
+
+use request::Request;
 
 const TODO: &str = "TODO";
 
-pub fn contains_todo(line: &str) -> bool {
+pub fn read_file(path: &str, request: &Request) -> io::Result<()> {
+    let file = File::open(path)?;
+    let buffer = BufReader::new(file);
+
+    let mut line_number = 0;
+    for line_option in buffer.lines() {
+        let line = line_option.unwrap();
+        line_number += 1;
+
+        if parse::contains_todo(&line) {
+            let params = Request::build_params(
+                parse::extract_title(&line),
+                parse::create_description(&line_number, path)
+            );
+            let result = request.create_issue(params);
+            println!("{:?}", result);
+        }
+    }
+
+    Ok(())
+}
+
+fn contains_todo(line: &str) -> bool {
     let comment = match line.find("//") {
         Some(value) => Some(value),
         None => line.find("#"),
@@ -18,7 +47,7 @@ pub fn contains_todo(line: &str) -> bool {
     false
 }
 
-pub fn extract_title(line: &str) -> String {
+fn extract_title(line: &str) -> String {
     let vec: Vec<&str> = line.split(TODO).collect();
     let after_todo = vec[1];
     let title = if after_todo.starts_with(":") {
@@ -30,7 +59,7 @@ pub fn extract_title(line: &str) -> String {
     String::from(title)
 }
 
-pub fn create_description(line_number: &u32, file_path: &str) -> String {
+fn create_description(line_number: &u32, file_path: &str) -> String {
     format!("Found a TODO comment on line {} of file {}",
             line_number, file_path).to_string()
 }
